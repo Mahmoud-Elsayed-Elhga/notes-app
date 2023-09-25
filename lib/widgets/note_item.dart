71940c1fbc13bd50f7d6a42f78856ab0acb26b36
Mahmoud Elsayed <mahmoudsayed9173@gmail.com>
2023-09-26 02:48:22 +0300

    created custom NoteItem

---
+++ lib/widgets/note_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 20, bottom: 20, left: 12),
+      decoration: BoxDecoration(
+          color: Colors.green, borderRadius: BorderRadius.circular(18)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                'Build your career with Mahmoud Elsayed',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.6), fontSize: 20),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete, size: 30, color: Colors.black)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: Text('May21,2023',
+                style: TextStyle(color: Colors.black.withOpacity(0.6))),
+          )
+        ],
+      ),
+    );
+  }
+}
