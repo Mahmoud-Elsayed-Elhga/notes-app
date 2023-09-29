26d9a6ac164942a3a41a0236b2ed9dd0ad09afd3
Mahmoud Elsayed <mahmoudsayed9173@gmail.com>
2023-09-29 11:48:26 +0300

    created NoteModel

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'custom_search_button.dart';
+import 'custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key, required this.text, required this.icon});
